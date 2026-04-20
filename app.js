2a6af45318d5d464f25d058f6cc5100e84b15bc7
nghiangotngao27-glitch <nghiangotngao27@gmail.com>
2026-04-20 21:14:47 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 function goDiscover() {
   const time = document.getElementById("timeInput").value;
   const interest = document.getElementById("interestSelect").value;
+  const result = document.getElementById("result");
 
   if (!time) {
-    document.getElementById("result").innerText = "Vui lòng nhập thời gian.";
+    result.innerText = "Vui lòng nhập thời gian rảnh.";
     return;
   }
 
-  document.getElementById("result").innerText =
+  result.innerText =
     `Với ${time} rảnh và sở thích ${interest}, bạn có thể chọn một trải nghiệm ngắn phù hợp gần bạn.`;
 }
-``
